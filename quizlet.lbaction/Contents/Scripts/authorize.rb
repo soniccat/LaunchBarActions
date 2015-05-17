5b5c28a7cda997b691a1a9a15b8f5a1ffd097a6e
Alexey Glushkov <soniccat@list.ru>
2015-05-17 14:13:49 +0300

    removed quizlet action

--- quizlet.lbaction/Contents/Scripts/authorize.rb
+++
@@ -1,9 +0,0 @@
-require 'uri'
-require "base64"
-require 'json'
-require 'time'
-require './quizlet.rb'
-
-authorizer = QuizletAuthorizer.new
-performer = CommandPerformer.new(authorizer)
-performer.authorizeIfNeeded()
